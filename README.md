5c424bc1ca8fd30e2be5c6cc58abef2c5b4facd9
cmarshall282 <christopherscottmarshall@gmail.com>
2019-04-22 13:59:53 -0500

    Added replace_text and update_file functions to DocsWriter. Organized into a class

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Shawnipedia
-"Just answer the question correctly" - Shawn Meintz
+"Just answer the question correctly" - Shawn Meintz<br/>
 This is a bot to help out with the demanding homework loads of AP World History taught by the one and only Shawn Meintz.
--- docswriter.py
+++ docswriter.py
@@ -6,41 +6,62 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
+class DocsWriter():
+    def __init__(self):
+        '''
+        Authenticates the DocsWriter.
+        '''
+        self.SCOPES = ['https://www.googleapis.com/auth/documents']
+        self.DOCUMENT_ID = '1dRZAWl_b9RqvrvIRM5q5XxtE5NcmeopazRk23qJshzk'
 
-# The ID of a sample document.
-DOCUMENT_ID = '1dRZAWl_b9RqvrvIRM5q5XxtE5NcmeopazRk23qJshzk'
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                creds = flow.run_local_server()
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
 
-def main():
-    """Shows basic usage of the Docs API.
-    Prints the title of a sample document.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server()
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+        self.service = build('docs', 'v1', credentials=creds)
+        self.requests = []
 
-    service = build('docs', 'v1', credentials=creds)
+    def replace_text(self, old_text, new_text, case_sensitive = 'false'):
+        '''
+        This function appends a replace text request to the list of reqests.
+        
+        old_text - Text that will be replaced.
+        new_text - Text that is replacing.
+        case_sensitive - Either 'true' or 'false'. old_text will be checked as case sensitive or not.
+        '''
 
-    # Retrieve the documents contents from the Docs service.
-    document = service.documents().get(documentId=DOCUMENT_ID).execute()
+        if case_sensitive != 'true' or case_sensitive != 'false':
+            return
 
-    print('The title of the document is: {}'.format(document.get('title')))
+        self.requests.append(
+            {
+                'replaceAllText': {
+                    'containsText': {
+                        'text': old_text,
+                        'matchCase': case_sensitive
+                    },
+                    'replaceText': new_text
+                }
+            }
+        )
 
-
-if __name__ == '__main__':
-    main()
+    def update_file(self):
+        '''
+        This function updates the file that is being worked with.
+        '''
+        result = self.service.documents().batchUpdate(documentId=self.DOCUMENT_ID, body={'requests': self.requests}).execute()
