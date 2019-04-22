aca71536da3da05e651fac01ea156e45e5db2f43
Ben <bvazzana672@kirksville.k12.mo.us>
2019-04-22 14:27:13 -0500

    implemented get_cards() + docstring

--- quizlet.py
+++ quizlet.py
@@ -23,17 +23,19 @@ class Quizlet():
                 item = str(card)[43::][:-7] # Slicing to remove html text
                 self.cards.append(item)
     def get_dict(self):
-        for url in self.urls:
-            req = requests.get(url)
-            soup = BeautifulSoup(req.text, 'html.parser')
+        """
+        Usage: [Quizlet instance].get_dict()
 
-            cards = soup.find_all('span', class_='TermText notranslate lang-en')
-            i = 0
-            for card in cards:
-                term = str(card)[43::][:-7]
-                if i % 2 == 0:
-                    self.term_dict[term] = str(cards[i + 1])[43::][:-7]
-                    i += 1
-                else:
-                    i += 1
-                    continue
+        Initializes the dictionary in which the terms are stored as keys,
+        and the definitions are stored as values in the dictionary
+        """
+        if self.cards == []:
+            self.get_cards()
+        i = 0
+        for card in self.cards:
+            if i % 2 == 0:
+                self.term_dict[card] = self.cards[i + 1]
+                i += 1
+            else:
+                i += 1
+                continue
